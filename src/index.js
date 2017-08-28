7145ea8ad8e2f73fc7a891dcab2d42eaabf76cf6
Gord Bolli <gord@bolli.ca>
2017-08-28 11:20:56 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj6wg1ct31fh00127ekf4uk11'
+})
+
+const client = new ApolloClient({
+  networkInterface
+})
+
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  , document.getElementById('root'));
+registerServiceWorker();
