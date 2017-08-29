382fb20e20c7dfd4700899521e2f0d34d3131512
Gord Bolli <gord@bolli.ca>
2017-08-28 17:17:33 -0700

    added authentication

--- src/index.js
+++ src/index.js
@@ -5,10 +5,22 @@ import './styles/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+import { GC_AUTH_TOKEN } from './constants'
 
 const networkInterface = createNetworkInterface({
   uri: 'https://api.graph.cool/simple/v1/cj6wg1ct31fh00127ekf4uk11'
 })
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
 
 const client = new ApolloClient({
   networkInterface
